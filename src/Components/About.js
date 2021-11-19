7a6db38c378c10a9069c422fe3ba3ae8cae05e02
JeanVPacheco <jeanvitorp@gmail.com>
2021-11-18 21:20:08 -0300

    Adding from another repo

---
+++ src/Components/About.js
@@ -0,0 +1,32 @@
+function About() {
+  return (
+    <section className="about-section">
+      <div className="about-personal-info">
+        <div>
+          <h1>Jean Vitor Pacheco</h1>
+          <h2>Front-end web developer</h2>
+          <h2>jeanvitorp@gmail.com</h2>
+        </div>
+        <div className="picture-placeholder" />
+      </div>
+      <div className="about-text">
+        <div className="about-title">
+          <h1>About me</h1>
+          <hr />
+        </div>
+        <p>
+          I have had a lot of diverse opportunities as an intern: a law firm, UFSC&apos;s internal
+          affairs office, Municipal Court of Palhoça and a bank (BRDE). Through these opportunities
+          I could work in the public and private sector, and different law contexts as well.
+        </p>
+        <p>
+          While working at my day to day activities, I realized that I could automate a lot of what
+          I was doing, and it sparked an instering in learning basic concepts of programming.
+          Nowadays I am an aspiring full-stack web developer, studying at Trybe.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <>
+      <nav>
+        <div className="yellow-bar" />
+        <ul>
+          <li className="header-component">About</li>
+          <li className="header-component">Skills</li>
+          <li className="header-component">Work</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
