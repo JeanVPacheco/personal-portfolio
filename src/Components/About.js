425d179f0a9fba42a1290b828e771229b9d965bb
JeanVPacheco <jeanvitorp@gmail.com>
2022-01-25 10:42:06 -0300

    Adding a color to button hover and changing resume

--- src/Components/About.js
+++ src/Components/About.js
@@ -47,7 +47,7 @@ function About() {
                   </div>
                 </button>
               </a>
-              <a href="/CURRICULO-JEAN-VITOR-PACHECO.pdf" target="_blank" rel="noreferrer" download>
+              <a href="/curriculo_jean_vitor_pacheco.pdf" target="_blank" rel="noreferrer" download>
                 <button className="btn">
                   <div className="about-button-content">
                     <img src={pdf} className="about-logo" alt="pdf logo" />
